aa9e0d69458029b6b94a438b7906f9885e3d8901
justin <justin.yikman@gmail.com>
2022-03-25 21:33:15 -0600

    Rendering Elements learning

--- my-first-react-app/src/index.js
+++ my-first-react-app/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez',
+};
+const formatName = (user) => {
+  return user.firstName + ' ' + user.lastName;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const tick = () => {
+  const element = (
+    <div>
+      <h1>Hello, {formatName(user)}!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+
+  ReactDOM.render(
+    <React.StrictMode>{element}</React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+setInterval(tick, 1000);
